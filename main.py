7fb4cd39607f0c9eee630c3a1158ba9d9c8f7f91
SadiqCafarli61 <sadiqcafarlidret@gmail.com>
2023-12-24 14:58:05 +0400

    piton

---
+++ main.py
@@ -0,0 +1,34 @@
+
+number1 = 23
+number2 = "23"
+
+print(abs(-2.24))
+print(19/2)
+
+num1 = 25/4
+print(round(num1))
+
+print((2+4) * 8+3)
+
+
+a=1
+b=a
+print(b)
+
+count1 = "100"
+count2 = 100
+print(type(count2))
+print(count1 == count2)
+
+# false  
+
+print(count1 != count2)
+# true
+
+count3 = int(count1)
+print(count3 == count2)
+
+count3 =round(-4.5)
+print(count3)
+
+
